8e7f55f8685b4e2824d60fcfac86225f16ad3779
Ilya Beliaev <ib@terralink.de>
2016-01-29 09:33:11 +0100

    added metadata

---
+++ metadata.php
@@ -0,0 +1,24 @@
+<?php
+$sMetadataVersion = "1.1";
+
+$aModule = array(
+    "id"          => "ib_DependencyManager",
+    "title"       => "OXID-DependencyManager",
+    "description" => "Abhängigkeitsverwaltung für OXID CE/PE/EE",
+    "thumbnail"   => "",
+    "version"     => "1.0.0",
+    "author"      => "Ilya Beliaev",
+    "url"         => "http://blog.php-dev.info",
+    "email"       => "info@php-dev.info",
+    "extend"      => array(
+        
+    ),
+    "files"       => array(
+    ),
+    "settings"    => array(
+        
+    ),
+    "templates"   => array(),
+    "blocks"      => array(),
+    "events"      => array(),
+);
